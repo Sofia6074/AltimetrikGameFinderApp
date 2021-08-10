ef6ffe178e1533aa84b3bdf3b01fd8ffef646f4e
Sofía Martínez <Sofijm6074@gmail.com>
2021-08-10 19:36:47 -0300

    change view functionality created and tested

--- mainMenuScript.js
+++ mainMenuScript.js
@@ -1,15 +1,24 @@
 window.addEventListener("load", start);
 
 function start() {
-    document.querySelector(".threeColumnView").addEventListener("click", threeColumnView);
-    document.querySelector(".singleColumnView").addEventListener("click", singleColumnView);
+    document.querySelector(".tripleColumnViewButton__svg").addEventListener("click", tripleColumnView);
+    document.querySelector(".singleColumnViewButton__svg").addEventListener("click", singleColumnView);
 }
 
-function threeColumnView(){
-    console.log("threeColumnView clicked");
+function tripleColumnView(){
+    document.querySelector(".singleColumnViewButton__svg").setAttribute("style", "fill:#303030;");
+    document.querySelector(".tripleColumnViewButton__svg").setAttribute("style", "fill:#515151;");
+    let cardElements = document.querySelectorAll(".card");
+    for (let i = 0; i < cardElements.length; i++) {
+        cardElements[i].classList.remove("singleColumnView");
+    }
 }
 
 function singleColumnView(){
-    console.log("singleColumnView clicked");
-    console.log(document.querySelectorAll(".cardInfo"))
+    document.querySelector(".singleColumnViewButton__svg").setAttribute("style", "fill:#515151;");
+    document.querySelector(".tripleColumnViewButton__svg").setAttribute("style", "fill:#303030;");
+    let cardElements = document.querySelectorAll(".card");
+    for (let i = 0; i < cardElements.length; i++) {
+        cardElements[i].classList.add("singleColumnView");
+    }
 }
