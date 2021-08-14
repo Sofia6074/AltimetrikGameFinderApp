e72482072ce802a770839498038eac355f12d6f5
Sofía Martínez <Sofijm6074@gmail.com>
2021-08-14 01:08:11 -0300

    mobile search bar added

--- mainMenuScript.js
+++ mainMenuScript.js
@@ -3,26 +3,10 @@ window.addEventListener("load", start);
 function start() {
     document.querySelector(".hamburgerMenu__svg").addEventListener("click",openMenu);
     document.querySelector(".clickContainer--tablet").addEventListener("click",closeMenu);
+    document.querySelector(".searchLens--mobile__svg").addEventListener("click",openSearchBar);
+    document.querySelector(".header__clickContainer--mobile").addEventListener("click",closeSearchBar);
     document.querySelector(".tripleColumnViewButton__svg").addEventListener("click", tripleColumnView);
     document.querySelector(".singleColumnViewButton__svg").addEventListener("click", singleColumnView);
-}
-
-function tripleColumnView(){
-    document.querySelector(".singleColumnViewButton__svg").setAttribute("style", "fill:#303030;");
-    document.querySelector(".tripleColumnViewButton__svg").setAttribute("style", "fill:#515151;");
-    let cardElements = document.querySelectorAll(".card");
-    for (let i = 0; i < cardElements.length; i++) {
-        cardElements[i].classList.remove("singleColumnView");
-    }
-}
-
-function singleColumnView(){
-    document.querySelector(".singleColumnViewButton__svg").setAttribute("style", "fill:#515151;");
-    document.querySelector(".tripleColumnViewButton__svg").setAttribute("style", "fill:#303030;");
-    let cardElements = document.querySelectorAll(".card");
-    for (let i = 0; i < cardElements.length; i++) {
-        cardElements[i].classList.add("singleColumnView");
-    }
 }
 
 function openMenu(){
@@ -47,5 +31,40 @@ function closeMenu(){
 
     if (window.screen.width < 767){
         document.querySelector(".headerOptions__hamburgerMenu").classList.remove("show");
+    }
+}
+
+function openSearchBar(){
+    if (document.querySelector(".header__clickContainer--mobile").classList.contains("show")){
+        closeSearchBar();
+    }
+    else{
+        document.querySelector(".header__clickContainer--mobile").classList.add("show");
+        document.querySelector(".header__searchBarContainer--mobile").classList.add("show");
+        document.querySelector("header").setAttribute("style", "height:166px");
+    }
+}
+
+function closeSearchBar(){
+    document.querySelector(".header__clickContainer--mobile").classList.remove("show");
+    document.querySelector(".header__searchBarContainer--mobile").classList.remove("show");
+    document.querySelector("header").removeAttribute("style", "height:166px");
+}
+
+function tripleColumnView(){
+    document.querySelector(".singleColumnViewButton__svg").setAttribute("style", "fill:#303030;");
+    document.querySelector(".tripleColumnViewButton__svg").setAttribute("style", "fill:#515151;");
+    let cardElements = document.querySelectorAll(".card");
+    for (let i = 0; i < cardElements.length; i++) {
+        cardElements[i].classList.remove("singleColumnView");
+    }
+}
+
+function singleColumnView(){
+    document.querySelector(".singleColumnViewButton__svg").setAttribute("style", "fill:#515151;");
+    document.querySelector(".tripleColumnViewButton__svg").setAttribute("style", "fill:#303030;");
+    let cardElements = document.querySelectorAll(".card");
+    for (let i = 0; i < cardElements.length; i++) {
+        cardElements[i].classList.add("singleColumnView");
     }
 }
